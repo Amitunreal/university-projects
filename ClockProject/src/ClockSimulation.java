00fc3ecd0e89c6f5ef97e347a6bb66149f28ef26
Amit1.Singh <Amit1.Singh@LT0013140>
2016-02-16 23:09:41 +0530

    Comment has been provide in ClockSimulation class.

--- ClockProject/src/ClockSimulation.java
+++ ClockProject/src/ClockSimulation.java
@@ -1,7 +1,21 @@
+/**
+ * 
+ * @author Jace Brady
+ * 
+ *         ClockSimulation class is responsible for providing all the
+ *         instructions that are required to test the project and provide the
+ *         out put in s specific format.
+ *
+ */
 public class ClockSimulation {
 
+	// A bag that will contain instance of each type of clock. [POLYMORPHISM]
 	private Bag<Clock> bag;
 
+	/**
+	 * Constructor of ClockSimulation Responsible for creation of each type of
+	 * clock instances and adding them into a container called bag
+	 */
 	public ClockSimulation() {
 		bag = new Bag<>();
 		bag.add(new AtomicClock(Clock.ClockType.quantum, 0.0));
@@ -12,9 +26,10 @@ public class ClockSimulation {
 	}
 
 	public static void main(String[] args) {
-		ClockSimulation cs = new ClockSimulation();
-		while (!cs.bag.isEmpty()) {
-			Clock c = cs.bag.remove();
+		ClockSimulation cs = new ClockSimulation(); // Instance of
+													// ClockSimulation class
+		while (!cs.bag.isEmpty()) { // Check whether bag is empty
+			Clock c = cs.bag.remove(); // Removes a clock one by one
 			System.out.println("*** Clock Simulations, Drift Per Second: " + c.time.getDriftPerSecond() + " ***\n");
 			printOutput(c, "starting  ", 0);
 			printOutput(c, "after 1 day  ", 86400);
@@ -25,11 +40,22 @@ public class ClockSimulation {
 		}
 	}
 
+	/**
+	 * 
+	 * @param c
+	 *            The instance of a clock
+	 * @param st
+	 *            A string that must be prepended before calling display method
+	 *            of a particular clock
+	 * @param seconds
+	 *            The amount of time in seconds for which a particular clock
+	 *            must tick before displaying the time
+	 */
 	public static void printOutput(Clock c, String st, int seconds) {
 		System.out.print(String.format("%15s", st));
 		for (int i = 0; i < seconds; i++)
-			c.tick();
-		c.display();
-		c.reset();
+			c.tick(); // Tick the clock for one second
+		c.display(); // Display the clock's time in specific manner
+		c.reset(); // Reset the time of a clock
 	}
 }
